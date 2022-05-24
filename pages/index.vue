3fed41598cf484ce0a10eec2e6989d7befe37c53
Radical <benjaminjohnsen.bgj@gmail.com>
2022-05-24 09:45:40 +0200

    Add an about me to index

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,16 @@
     <h1 class="headerh1">Radical</h1>
     <p class="headertext">Programmer, Computer Enthusiast, Linux Enthusiast.</p>
   </div>
+  <div class="maindiv">
+    <h1 class="mainheader">About Me</h1>
+  </div>
+  <div class="abtdiv">
+    <p class="abttext">
+      Hi👋, my name is <b>Benjamin</b>, I live in <b>Norway</b>🇳🇴 and I'm studying🧑‍🎓 to become a <b>Software Developer</b>🧑‍💻.<br/><br/>
+      I'm interested in <b>Backend Development</b> and my favorite languages at the moment are <b>Rust</b>🦀, <b>JavaScript</b> and <b>Python</b>🐍.<br/><br/>
+      This website has a collection of all the projects that I've worked on in the past few months.
+    </p>
+  </div>
   <div class="maindiv" id="personal">
     <h1 class="mainheader">Personal</h1>
   </div>

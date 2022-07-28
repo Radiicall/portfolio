bbdac89103a3242152c4cb025d3a4f951637ec5e
Radical <benjaminjohnsen.bgj@gmail.com>
2022-07-28 17:59:23 +0200

    update navbar in a questionable way

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,10 +1,13 @@
 <template>
   <link rel="stylesheet" href="/assets/css/navbar.component.css"> 
   <nav class="navbar-menu">
-    <div>
+    <div class="navbar-div1">
       <a href="/" class="navbar-item">Home</a>
       <a href="/projects" class="navbar-item">Projects</a>
       <a href="/#personal" class="navbar-item">Personal</a>
+    </div>
+    <div class="navbar-div2">
+      <a href="https://rajikaru.xyz" class="navbar-item">SearXNG</a>
     </div>
   </nav>
 </template>
